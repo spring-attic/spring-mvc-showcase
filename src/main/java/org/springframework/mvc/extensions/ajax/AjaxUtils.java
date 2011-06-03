14f5c727f8091aec4176c9215b6d8f99efb6ac24
Keith Donald <keith.donald@springsource.com>
2011-06-03 14:34:43 -0400

    simplified ajax utils

--- src/main/java/org/springframework/mvc/extensions/ajax/AjaxUtils.java
+++ src/main/java/org/springframework/mvc/extensions/ajax/AjaxUtils.java
@@ -1,15 +1,16 @@
 package org.springframework.mvc.extensions.ajax;
 
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.context.request.WebRequest;
 
 public class AjaxUtils {
 
-	public static boolean isAjaxRequest(String requestedWith) {
+	public static boolean isAjaxRequest(WebRequest webRequest) {
+		String requestedWith = webRequest.getHeader("X-Requested-With");
 		return requestedWith != null ? "XMLHttpRequest".equals(requestedWith) : false;
 	}
 
-	public static boolean isAjaxUploadRequest(HttpServletRequest request) {
-		return request.getParameter("ajaxUpload") != null;
+	public static boolean isAjaxUploadRequest(WebRequest webRequest) {
+		return webRequest.getParameter("ajaxUpload") != null;
 	}
 	
 	private AjaxUtils() {}
--- src/main/java/org/springframework/samples/mvc/fileupload/FileUploadController.java
+++ src/main/java/org/springframework/samples/mvc/fileupload/FileUploadController.java
@@ -2,17 +2,15 @@ package org.springframework.samples.mvc.fileupload;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.mvc.extensions.ajax.AjaxUtils;
 import org.springframework.mvc.extensions.flash.FlashMap.Message;
 import org.springframework.mvc.extensions.flash.FlashMap.MessageType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
@@ -20,16 +18,16 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileUploadController {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public void fileUploadForm(@RequestHeader(value="X-Requested-With", required=false) String requestedWith, Model model) {
-		model.addAttribute("ajaxRequest", AjaxUtils.isAjaxRequest(requestedWith));	
+	public void fileUploadForm(WebRequest webRequest, Model model) {
+		model.addAttribute("ajaxRequest", AjaxUtils.isAjaxRequest(webRequest));	
 	}
 
 	@RequestMapping(method=RequestMethod.POST)
-	public void processUpload(@RequestParam MultipartFile file, HttpServletRequest request, Model model) throws IOException {
+	public void processUpload(@RequestParam MultipartFile file, WebRequest webRequest, Model model) throws IOException {
 		String message = "File '" + file.getOriginalFilename() + "' uploaded successfully";
 		// prepare model for rendering success message in this request
 		model.addAttribute("message", new Message(MessageType.success, message));
-		model.addAttribute("ajaxRequest", AjaxUtils.isAjaxUploadRequest(request));			
+		model.addAttribute("ajaxRequest", AjaxUtils.isAjaxUploadRequest(webRequest));			
 	}
 	
 }
