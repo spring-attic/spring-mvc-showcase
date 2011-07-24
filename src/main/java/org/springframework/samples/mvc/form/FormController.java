dcb116aeed16754cb2eaab3009164c04da9c2e9e
Stefano Locati <stefano@obliquid.it>
2011-07-24 21:20:17 +0800

    added dependency spring-web, it should be automatically included,
    but since I was trying to solve a ClassNotFoundException, I've
    added it. I found the solution on stackoverflow.com #2595315
    Modified a couple of if/else to remove eclipse warnings

--- src/main/java/org/springframework/samples/mvc/form/FormController.java
+++ src/main/java/org/springframework/samples/mvc/form/FormController.java
@@ -18,34 +18,34 @@ import org.springframework.web.context.request.WebRequest;
 @RequestMapping("/form")
 public class FormController {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public void form(WebRequest webRequest, HttpSession session, Model model) {
-		FormBean form = (FormBean) session.getAttribute("form");
-		model.addAttribute(form != null ? form : new FormBean());
-		model.addAttribute("ajaxRequest", AjaxUtils.isAjaxRequest(webRequest));
-	}
+    @RequestMapping(method = RequestMethod.GET)
+    public void form(WebRequest webRequest, HttpSession session, Model model) {
+        FormBean form = (FormBean) session.getAttribute("form");
+        model.addAttribute(form != null ? form : new FormBean());
+        model.addAttribute("ajaxRequest", AjaxUtils.isAjaxRequest(webRequest));
+    }
 
-	@RequestMapping(method=RequestMethod.POST)
-	public String processSubmit(@Valid FormBean form, BindingResult result, WebRequest webRequest, HttpSession session, Model model) {
-		if (result.hasErrors()) {
-			model.addAttribute("ajaxRequest", AjaxUtils.isAjaxRequest(webRequest));
-			return null;
-		}
-		// simply store form bean in the session for demo purposes, typically you would save form bean values to a db
-		session.setAttribute("form", form);
-		String message = "Form submitted successfully.  Bound " + form;
-		// success response handling
-		if (AjaxUtils.isAjaxRequest(webRequest)) {
-			// prepare model for rendering success message in this request
-			model.addAttribute("message", new Message(MessageType.success, message));
-			model.addAttribute("ajaxRequest", true);
-			return null;
-		} else {
-			// store a success message for rendering on the next request after redirect
-			FlashMap.setSuccessMessage(message);
-			// redirect back to the form to render the success message along with newly bound values
-			return "redirect:/form";			
-		}
-	}
-	
+    @RequestMapping(method = RequestMethod.POST)
+    public String processSubmit(@Valid FormBean form, BindingResult result, WebRequest webRequest,
+            HttpSession session, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("ajaxRequest", AjaxUtils.isAjaxRequest(webRequest));
+            return null;
+        }
+        // simply store form bean in the session for demo purposes, typically you would save form bean values to a db
+        session.setAttribute("form", form);
+        String message = "Form submitted successfully.  Bound " + form;
+        // success response handling
+        if (AjaxUtils.isAjaxRequest(webRequest)) {
+            // prepare model for rendering success message in this request
+            model.addAttribute("message", new Message(MessageType.success, message));
+            model.addAttribute("ajaxRequest", true);
+            return null;
+        }
+        // store a success message for rendering on the next request after redirect
+        FlashMap.setSuccessMessage(message);
+        // redirect back to the form to render the success message along with newly bound values
+        return "redirect:/form";
+    }
+
 }
