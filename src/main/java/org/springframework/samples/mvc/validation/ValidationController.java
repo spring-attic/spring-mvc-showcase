648b70cd82e3bd65ac497158240954490235c8c1
deepanshuPC <dpanshu.cool@gmail.com>
2015-02-20 19:20:15 +0530

    demonstrate use of regex for validating bean in Validation tab

--- src/main/java/org/springframework/samples/mvc/validation/ValidationController.java
+++ src/main/java/org/springframework/samples/mvc/validation/ValidationController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ValidationController {
 
 	// enforcement of constraints on the JavaBean arg require a JSR-303 provider on the classpath
-	
 	@RequestMapping("/validate")
 	public @ResponseBody String validate(@Valid JavaBean bean, BindingResult result) {
 		if (result.hasErrors()) {
@@ -20,5 +19,4 @@ public class ValidationController {
 			return "No errors";
 		}
 	}
-
 }
