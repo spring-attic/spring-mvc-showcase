1e16ae88e36a02bae391fceb8c855e21ad07cca3
rajadilipkolli <rajadilipkolli@gmail.com>
2017-02-07 00:12:41 +0530

    upgrade to latest available jars and fix deprecated, unused imports

--- src/test/java/org/springframework/samples/mvc/response/ResponseControllerTests.java
+++ src/test/java/org/springframework/samples/mvc/response/ResponseControllerTests.java
@@ -1,7 +1,6 @@
 package org.springframework.samples.mvc.response;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
