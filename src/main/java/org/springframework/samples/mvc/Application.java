da185d4622d1c9b40899e349dcf448c0e6149da2
rajadilipkolli <rajadilipkolli@gmail.com>
2017-02-17 01:30:38 +0530

    Converts from spring to spring-boot application

---
+++ src/main/java/org/springframework/samples/mvc/Application.java
@@ -0,0 +1,26 @@
+package org.springframework.samples.mvc;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return configureApplication(builder);
+    }
+
+    public static void main(String[] args) {
+        configureApplication(new SpringApplicationBuilder()).run(args);
+    }
+
+    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class).bannerMode(Banner.Mode.OFF);
+    }
+
+}
--- src/main/java/org/springframework/samples/mvc/redirect/RedirectController.java
+++ src/main/java/org/springframework/samples/mvc/redirect/RedirectController.java
@@ -1,8 +1,7 @@
 package org.springframework.samples.mvc.redirect;
 
-import javax.inject.Inject;
+import java.time.LocalDate;
 
-import org.joda.time.LocalDate;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,6 @@ public class RedirectController {
 	
 	private final ConversionService conversionService;
 
-	@Inject
 	public RedirectController(ConversionService conversionService) {
 		this.conversionService = conversionService;
 	}
@@ -27,13 +25,13 @@ public class RedirectController {
 	@RequestMapping(value="/uriTemplate", method=RequestMethod.GET)
 	public String uriTemplate(RedirectAttributes redirectAttrs) {
 		redirectAttrs.addAttribute("account", "a123");  // Used as URI template variable
-		redirectAttrs.addAttribute("date", new LocalDate(2011, 12, 31));  // Appended as a query parameter
+		redirectAttrs.addAttribute("date", LocalDate.of(2011, 12, 31));  // Appended as a query parameter
 		return "redirect:/redirect/{account}";
 	}
 
 	@RequestMapping(value="/uriComponentsBuilder", method=RequestMethod.GET)
 	public String uriComponentsBuilder() {
-		String date = this.conversionService.convert(new LocalDate(2011, 12, 31), String.class);
+		String date = this.conversionService.convert(LocalDate.of(2011, 12, 31), String.class);
 		UriComponents redirectUri = UriComponentsBuilder.fromPath("/redirect/{account}").queryParam("date", date)
 				.build().expand("a123").encode();
 		return "redirect:" + redirectUri.toUriString();
