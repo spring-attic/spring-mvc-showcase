0d88c86a6dd364c25d57c09cd225265bba7acd34
rajadilipkolli <rajadilipkolli@gmail.com>
2017-02-17 01:55:27 +0530

    Fix xmlunit version with master and removed unused annotation

--- src/main/java/org/springframework/samples/mvc/Application.java
+++ src/main/java/org/springframework/samples/mvc/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class Application extends SpringBootServletInitializer {
 
     @Override
